5e560b93d1b0f1cc463917da98b3668a5a569cb4
Jonathan Cole <jon@jcole.io>
2025-07-11 22:22:37 -0400

    fixed rendering issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import * as _ from 'lodash'
-import { useMemo } from 'react'
 import Board from './Board';
+
+const board = <Board />
 
 function App() {
 	console.log("Rendering app")
-
-	const board = useMemo(() => <Board />, [])
 
 	return board
 }
--- src/Board.tsx
+++ src/Board.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import Card from "./Card";
 import Deck, { CardType } from "./Deck";
 import {range} from 'lodash'
@@ -11,89 +11,111 @@ const emptyGrid = range(0, ROWS).map(_ => {
 	return range(0, COLS).map(() => false)
 });
 
-console.log(emptyGrid)
+function deflatten(n: number): [number, number] {
+	var col = n % (ROWS-1);
+	var row = Math.floor(n / (COLS))
+	return [row, col];
+}
+
+function stepForward(pos: number[]) {
+	const [a, b, c, max] = pos
+	if (c < max-1) return [a, b, c+1, max];
+	else if (b < c-1) {
+		return [a, b+1, b+2, max];
+	} else if (a < b-1) {
+		return [a+1, a+2, a+3, max];
+	} else return null;
+}
+
+// 1 if all the same, -1 if all different, 0 otherwise
+function compareValues(v: number[]): number {
+	var results = v.reduce((agg, e) => {
+		if (agg[String(e)] === undefined) agg[String(e)] = 0;
+		agg[String(e)]++;
+		return agg;
+	}, {} as {[K: string]: number});
+	var keys = Object.keys(results);
+	// console.log(results)
+	// console.log(keys)
+	if (keys.length == 1) return 1;
+	else if (keys.length == v.length) return -1;
+	else return 0;
+}
+
+function checkSet(cards: CardType[][], newSelected: boolean[][]): boolean {
+	const cardsFiltered = cards.map((r, i) => r.filter((e, j) => newSelected[i][j]));
+	const selectedCards = cardsFiltered.flatMap(r => r);
+	// console.log(selectedCards)
+	if (selectedCards.length != 3) return false;
+
+
+	const colorMatch = compareValues(selectedCards.map(c => c.color))
+	const countMatch = compareValues(selectedCards.map(c => c.count))
+	const fillMatch = compareValues(selectedCards.map(c => c.fill))
+	const shapeMatch = compareValues(selectedCards.map(c => c.shape))
+
+	// console.log("colorMatch: ", colorMatch)
+	// console.log("countMatch: ", countMatch)
+	// console.log("fillMatch: ", fillMatch)
+	// console.log("shapeMatch: ", shapeMatch)
+
+
+	return (
+		colorMatch != 0 && countMatch != 0 && fillMatch != 0 && shapeMatch != 0
+	)
+}
 
 export default function Board() {
 	const [selectedCount, setSelectedCount] = useState(0)
 	
-	const deck = useMemo(() => new Deck(), []);
+	const [deck, setDeck] = useState(null as Deck | null)
 	const [cards, setCards] = useState([] as CardType[][])
 	const [selected, setSelected] = useState([] as boolean[][])
 
+	const [numberSets, setNumberSets] = useState(0)
+	const [cardsLeft, setCardsLeft] = useState(0)
+
 	useEffect(() => {
-		console.log("init cards")
+		const newDeck = new Deck();
+		console.log("init cards " + newDeck.cardsLeft())
 		const newCards: CardType[][] = []
 		const newSelected: boolean[][] = []
 		range(0, ROWS).forEach(row => {
-			newCards[row] = range(0, COLS).map(() => deck.draw())
+			newCards[row] = range(0, COLS).map(() => newDeck.draw())
 			newSelected[row] = range(0, COLS).map(() => false)
 		});
 		setCards(newCards)
 		setSelected(newSelected)
+		setCardsLeft(newDeck.cardsLeft())
+		setDeck(newDeck)
 		setTimeout(() => findAllSets(newCards), 20);
 		// range(0, 12).map(deflatten).forEach(s => console.log(s))
 	}, [])
 
-	function findAllSets(cards: CardType[][]) {
+
+	const findAllSets = useCallback((cards: CardType[][]) => {
 		var pos: number[] | null = [0,1,2,ROWS * COLS];
 		var count = 0;
 		while (pos != null) {
-			const selected = clone2dArray(emptyGrid);
+			const selectedInner = clone2dArray(emptyGrid);
 			const deflattened = [deflatten(pos[0]), deflatten(pos[1]), deflatten(pos[2])]
-			deflattened.forEach(point => selected[point[0]][point[1]] = true);
+			deflattened.forEach(point => selectedInner[point[0]][point[1]] = true);
 			// console.log(selected)
-			if (checkSet(cards, selected)) {
+			if (checkSet(cards, selectedInner)) {
 				count++;
 			} else {
 				// console.log("not a set: ", deflattened)
 			}
 			pos = stepForward(pos);
 		}
+		setNumberSets(count)
 		console.log("# of sets: " + count)
-	}
-
-	function deflatten(n: number): [number, number] {
-		var col = n % (ROWS-1);
-		var row = Math.floor(n / (COLS))
-		return [row, col];
-	}
-
-	function stepForward(pos: number[]) {
-		const [a, b, c, max] = pos
-		if (c < max-1) return [a, b, c+1, max];
-		else if (b < c-1) {
-			return [a, b+1, b+2, max];
-		} else if (a < b-1) {
-			return [a+1, a+2, a+3, max];
-		} else return null;
-	}
-
-	function checkSet(cards: CardType[][], newSelected: boolean[][]): boolean {
-		const cardsFiltered = cards.map((r, i) => r.filter((e, j) => newSelected[i][j]));
-		const selectedCards = cardsFiltered.flatMap(r => r);
-		// console.log(selectedCards)
-		if (selectedCards.length != 3) return false;
+	},[setNumberSets])
 
 
-		const colorMatch = compareValues(selectedCards.map(c => c.color))
-		const countMatch = compareValues(selectedCards.map(c => c.count))
-		const fillMatch = compareValues(selectedCards.map(c => c.fill))
-		const shapeMatch = compareValues(selectedCards.map(c => c.shape))
-
-		// console.log("colorMatch: ", colorMatch)
-		// console.log("countMatch: ", countMatch)
-		// console.log("fillMatch: ", fillMatch)
-		// console.log("shapeMatch: ", shapeMatch)
-
-
-		return (
-			colorMatch != 0 && countMatch != 0 && fillMatch != 0 && shapeMatch != 0
-		)
-	}
-
-
-	const onClick = (i: number, j: number) => () => {
+	const onClick = useCallback((i: number, j: number) => () => {
 		console.log("click")
+		if (deck === null) return;
 		const newSelected = clone2dArray(selected);
 		const newState = !selected[i][j];
 		newSelected[i][j] = newState;
@@ -117,6 +139,7 @@ export default function Board() {
 					setSelectedCount(0)
 					setSelected(newSelected);
 					console.log(deck.cardsLeft() + " cards left")
+					setCardsLeft(deck.cardsLeft())
 					setTimeout(() => findAllSets(newCards), 20);
 					return;
 				} else {
@@ -126,24 +149,9 @@ export default function Board() {
 			setSelected(newSelected);
 			setSelectedCount(newSelectedCount);
 		}
-		console.log(newSelectedCount);
+		// console.log(newSelectedCount);
 
-	}
-
-	// 1 if all the same, -1 if all different, 0 otherwise
-	function compareValues(v: number[]): number {
-		var results = v.reduce((agg, e) => {
-			if (agg[String(e)] === undefined) agg[String(e)] = 0;
-			agg[String(e)]++;
-			return agg;
-		}, {} as {[K: string]: number});
-		var keys = Object.keys(results);
-		// console.log(results)
-		// console.log(keys)
-		if (keys.length == 1) return 1;
-		else if (keys.length == v.length) return -1;
-		else return 0;
-	}
+	}, [selected, selectedCount, cards])
 
 	const result = useMemo(() => {
 		console.log("calc board")
@@ -170,8 +178,13 @@ export default function Board() {
 					})}
 				</tbody>
 			</table>
+			<br />
+			Number of sets: {numberSets}
+			<br />
+			<br />
+			Cards left: {cardsLeft}
 		</>},
-		[cards, selected]
+		[cards, selected, numberSets, cardsLeft, onClick]
 	);
 
 	return result;
